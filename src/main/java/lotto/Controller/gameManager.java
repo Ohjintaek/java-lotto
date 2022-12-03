959a774034502d8ce667e1da2a00f73d1f5293cb
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 15:42:54 +0900

    feat: Lotto의 결과를 판단하는 기능 추가

--- src/main/java/lotto/Controller/gameManager.java
+++ src/main/java/lotto/Controller/gameManager.java
@@ -3,6 +3,7 @@ package lotto.Controller;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Model.Lotto;
 import lotto.Model.LottoGame;
+import lotto.Model.Rank;
 import lotto.View.InputView;
 
 import java.util.List;
@@ -29,6 +30,35 @@ public class gameManager {
         int number = lottoGame.lottoNum;
         for (int count = 0; count < number; count++) {
             lottoGame.purchasedLotto.add(makeLotto());
+        }
+    }
+
+    private void judgeRank(List<Integer> myLotto, List<Integer> winningLotto, int bonusNumber) {
+        int count = 0;
+        for (int number : myLotto) {
+            if (winningLotto.contains(number)) {
+                count++;
+            }
+        }
+
+        if (count < 3) {
+            lottoGame.result.set(Rank.NOTHING.getRank(), lottoGame.result.get(Rank.NOTHING.getRank()));
+        }
+        if (count == 6) {
+            lottoGame.result.set(Rank.FIRST.getRank(), lottoGame.result.get(Rank.FIRST.getRank()));
+        }
+        if (count == 5) {
+            if (myLotto.contains(bonusNumber)) {
+                lottoGame.result.set(Rank.SECOND.getRank(), lottoGame.result.get(Rank.SECOND.getRank()));
+                return;
+            }
+            lottoGame.result.set(Rank.THIRD.getRank(), lottoGame.result.get(Rank.THIRD.getRank()));
+        }
+        if (count == 4) {
+            lottoGame.result.set(Rank.FOURTH.getRank(), lottoGame.result.get(Rank.FOURTH.getRank()));
+        }
+        if (count == 3) {
+            lottoGame.result.set(Rank.FIFTH.getRank(), lottoGame.result.get(Rank.FIFTH.getRank()));
         }
     }
 }
