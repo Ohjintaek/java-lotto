548f6e7e519cdeab296e4c5999ae8eb5109ed762
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 17:25:14 +0900

    feat: 숫자를 입력했는지 판단하는 기능 및 0으로 시작하는 입력인지 판단하는 기능 추가

--- src/main/java/lotto/Controller/Error.java
+++ src/main/java/lotto/Controller/Error.java
@@ -4,13 +4,21 @@ public class Error {
     private final static int MINLOTTONUM = 1;
     private final static int MAXLOTTOUM = 45;
 
-    public static boolean isValidRange(int number) {
-        if (number < MINLOTTONUM) {
-            return false;
+    public static void isValidNumber(String userInput) {
+        try {
+            Integer.valueOf(userInput);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
         }
-        if (number > MAXLOTTOUM) {
-            return false;
+
+        if (userInput.startsWith("0")) {
+            throw new IllegalArgumentException("[ERROR] 0으로 시작하지 않는 숫자를 입력해 주세요.");
         }
-        return true;
+    }
+
+    public static void isValidRange(int number) {
+        if (number < MINLOTTONUM || number < MAXLOTTOUM) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45까지의 숫자만 입력해주세요.");
+        }
     }
 }
