1f23ce34354b84e47a6d9f53072019ab5689446d
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 16:49:47 +0900

    feat: Application에서 필요한 클래스를 호출하고 게임을 시작하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.Controller.GameManager;
+import lotto.Model.LottoGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoGame lottogame = new LottoGame();
+        GameManager gameManager = new GameManager(lottogame);
+        gameManager.run();
     }
 }
