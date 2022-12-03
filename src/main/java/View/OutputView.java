999bafc25a9447be565f3659459f3bb0fc6eadaa
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 14:23:17 +0900

    feat: 당첨 통계를 출력하는 기능 추가

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -11,4 +11,17 @@ public class OutputView {
             System.out.println(lotto);
         }
     }
+
+    public static void showResult(List<Integer> result, float yield) {
+        System.out.println(
+                "당첨 통계\n" +
+                "---\n" +
+                String.format("3개 일치 (5,000원) - %d개", result.get(4)) +
+                String.format("4개 일치 (50,000원) - %d개", result.get(3)) +
+                String.format("5개 일치 (1,500,000dnjs) - %d개", result.get(2)) +
+                String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", result.get(1)) +
+                String.format("6개 일치 (2,000,000,000원) - %d개", result.get(0)) +
+                String.format("총 수익률은 %f입니다.", yield)
+        );
+    }
 }
