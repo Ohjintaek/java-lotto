f7830217d4684ba950e5701890ec3fbd8acdc4d6
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 16:48:55 +0900

    refactor: View 모델에서 사용자에게 보이는 문구를 요구사항대로 수정

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -6,18 +6,21 @@ public class InputView {
     public static String getBudget() {
         System.out.println("구입금액을 입력해 주세요.");
         String budget = Console.readLine();
+        System.out.println();
         return budget;
     }
 
     public static String getWinningLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningLotto = Console.readLine();
+        System.out.println();
         return winningLotto;
     }
 
     public static String getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
+        System.out.println();
         return bonusNumber;
     }
 }
--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -9,20 +9,22 @@ public class OutputView {
     public static void showPurchasedLotto(int number, List<Lotto> purchasedLotto) {
         System.out.println(String.format("%d개를 구매하였습니다.", number));
         for(Lotto lotto : purchasedLotto) {
-            System.out.println(lotto);
+            System.out.println(lotto.getNumbers());
         }
+        System.out.println();
     }
 
     public static void showResult(List<Integer> result, float yield) {
-        System.out.println(
+        System.out.println(String.format(
                 "당첨 통계\n" +
                 "---\n" +
-                String.format("3개 일치 (5,000원) - %d개", result.get(Rank.FIFTH.getRank())) +
-                String.format("4개 일치 (50,000원) - %d개", result.get(Rank.FOURTH.getRank())) +
-                String.format("5개 일치 (1,500,000dnjs) - %d개", result.get(Rank.THIRD.getRank())) +
-                String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", result.get(Rank.SECOND.getRank())) +
-                String.format("6개 일치 (2,000,000,000원) - %d개", result.get(Rank.FIRST.getRank())) +
-                String.format("총 수익률은 %f입니다.", yield)
+                "3개 일치 (5,000원) - %d개\n" +
+                "4개 일치 (50,000원) - %d개\n" +
+                "5개 일치 (1,500,000원) - %d개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" +
+                "6개 일치 (2,000,000,000원) - %d개\n" +
+                "총 수익률은 %.1f%%입니다.",
+                result.get(5), result.get(4), result.get(3), result.get(2), result.get(1), yield)
         );
     }
 }
