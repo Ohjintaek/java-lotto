bcd305ccf3a8d2f6ea4201b69700aa0fea12fa7f
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 14:05:21 +0900

    feat: 당첨 번호를 입력받는 기능 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -8,4 +8,10 @@ public class InputView {
         String budget = Console.readLine();
         return budget;
     }
+
+    public String getWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningLotto = Console.readLine();
+        return winningLotto;
+    }
 }
