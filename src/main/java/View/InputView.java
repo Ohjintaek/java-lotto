2f4ec8137e193029db619649522a5bc4e218f8f2
2014012606 <2014012606@hanyang.ac.kr>
2022-12-03 14:13:57 +0900

    refactor: InputView 클래스의 method를 static으로 변경

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -3,19 +3,19 @@ package View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String getBudget() {
+    public static String getBudget() {
         System.out.println("구입금액을 입력해 주세요.");
         String budget = Console.readLine();
         return budget;
     }
 
-    public String getWinningLotto() {
+    public static String getWinningLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningLotto = Console.readLine();
         return winningLotto;
     }
 
-    public String getBonusNumber() {
+    public static String getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
         return bonusNumber;
